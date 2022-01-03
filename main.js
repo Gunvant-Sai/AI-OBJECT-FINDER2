837e85ebc19b22c40afa92d20a481b1dc045ba84
Gunvant-Sai <79575963+Gunvant-Sai@users.noreply.github.com>
2022-01-03 14:02:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+var video = "";
+var status1 = "";
+
+function setup() 
+{
+    canvas = createCanvas(480,480);
+    canvas.center();
+
+    video = createCapture(480,480)
+    video.hide();
+}
+
+function Start(){
+    objectdecter = ml5.objectDetector("cocossd",modelLoaded);
+    document.getElementById("classes").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded()
+{
+    console.log("modelLoaded");
+    status1=true;
+}
+
+function draw() {
+    image(video,0,0,580,480);
+}
